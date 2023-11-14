74a904dd11692bbfed6fe24d04758942d5da2049
jhonatan <j.azevedo@ctrlplay.com.br>
2023-11-14 17:13:31 -0300

    saving consumable items update tests

--- code/motel/tests/test_consumable_item.py
+++ code/motel/tests/test_consumable_item.py
@@ -1,17 +1,131 @@
 from rest_framework.test import APITestCase
 from utils.factories.consumable_item_factory import ConsumableItemFactory
 
+
 class TestConsumableItem(APITestCase):
     def setUp(self):
         self.url = "/api/consumable-item/"
-        
+
     """
         Test Create
     """
-    
+
     def test_create(self):
         payload = {
-            
+            "name": "Brahma",
+            "price": 7.50,
+            "description": "Trincando de gelada",
         }
-    
-    
+
+        response = self.client.post(self.url, payload)
+        self.assertEquals(response.status_code, 201)
+
+        response_data = response.json()
+
+        self.assertTrue("id" in response_data)
+        self.assertEquals(response_data["name"], payload["name"])
+        self.assertEquals(response_data["price"], payload["price"])
+        self.assertEquals(response_data["description"], payload["description"])
+
+    def test_create_without_description(self):
+        payload = {
+            "name": "Suco",
+            "price": 2,
+        }
+
+        response = self.client.post(self.url, payload)
+        self.assertEquals(response.status_code, 201)
+
+    def test_try_create_without_data(self):
+        payload = {}
+
+        response = self.client.post(self.url, payload)
+        self.assertEquals(response.status_code, 400)
+
+    def test_try_create_without_name(self):
+        payload = {
+            "price": 2.0,
+        }
+
+        response = self.client.post(self.url, payload)
+        self.assertEquals(response.status_code, 400)
+
+        response_data = response.json()
+
+        self.assertEquals(len(response_data), 1)
+        self.assertEquals(response_data["name"][0], "This field is required.")
+
+    def test_try_create_without_price(self):
+        payload = {
+            "name": "Item",
+        }
+
+        response = self.client.post(self.url, payload)
+        self.assertEquals(response.status_code, 400)
+
+        response_data = response.json()
+
+        self.assertEquals(len(response_data), 1)
+        self.assertEquals(response_data["price"][0], "This field is required.")
+
+    """
+        Test Update
+    """
+
+    def test_update(self):
+        item = ConsumableItemFactory(
+            name="Just a Item",
+            price=10.30,
+            description="Simple Description",
+        )
+
+        payload = {
+            "name": "Another item",
+            "price": 32.9,
+            "description": "Another Description",
+        }
+
+        response = self.client.put(f"{self.url}{item.id}/", payload)
+        self.assertEquals(response.status_code, 200)
+
+        response_data = response.json()
+
+        self.assertEquals(response_data["name"], payload["name"])
+        self.assertEquals(response_data["price"], payload["price"])
+        self.assertEquals(response_data["description"], payload["description"])
+        
+    def test_update_name(self):
+        item = ConsumableItemFactory(name="First Name")
+        
+        payload = {
+            "name": "Another Name"
+        }
+        
+        response = self.client.patch(f"{self.url}{item.id}/", payload)
+        self.assertEquals(response.status_code, 200)
+        
+        self.assertEquals(response.json()["name"], payload["name"])
+        
+    def test_update_price(self):
+        item = ConsumableItemFactory(price=20)
+        
+        payload = {
+            "price": 39.9
+        }
+        
+        response = self.client.patch(f"{self.url}{item.id}/", payload)
+        self.assertEquals(response.status_code, 200)
+        
+        self.assertEquals(response.json()["price"], payload["price"])
+        
+    def test_update_description(self):
+        item = ConsumableItemFactory(description="Simple Description")
+        
+        payload = {
+            "description": "Full Description"
+        }
+        
+        response = self.client.patch(f"{self.url}{item.id}/", payload)
+        self.assertEquals(response.status_code, 200)
+        
+        self.assertEquals(response.json()["description"], payload["description"])
