875e0ace996b731f2eeb620d1c48697ada5d25dd
jhonatan <j.azevedo@ctrlplay.com.br>
2023-11-17 12:07:34 -0300

    add icon on add accommodation

--- frontend/src/pages/Accommodation.tsx
+++ frontend/src/pages/Accommodation.tsx
@@ -3,6 +3,8 @@ import AccommodationModel from "../models/AccommodationModel";
 import AddAccommodation from "../components/accommodation/AddAccommodation";
 import Add from "../components/Add";
 import Collapse from "../components/Collapse";
+import { FaSquarePlus } from "react-icons/fa6";
+import { FaSquareMinus } from "react-icons/fa6";
 
 import accommodationDumy from "../data/accommodationDumy";
 import roomDumy from "../data/roomDumy";
@@ -14,22 +16,31 @@ export default function Accommodation() {
   return (
     <div className="flex flex-col gap-4 items-center">
       <h1 className="text-3xl font-bold tracking-wider">Acomodações</h1>
-      <button onClick={() => setOpenAdd(!openAdd)} className="p-4 bg-green-300">
-        +
+      <button onClick={() => setOpenAdd(!openAdd)} className="">
+        {openAdd ? (
+          <FaSquareMinus className="text-4xl" fill="red" />
+        ) : (
+          <FaSquarePlus className="text-4xl" fill="green" />
+        )}
       </button>
 
       {openAdd && (
         <div className="w-full">
-          <Add children={<AddAccommodation rooms={roomDumy} consumableItems={consumableItemDumy}/>}/>
+          <Add
+            children={
+              <AddAccommodation
+                rooms={roomDumy}
+                consumableItems={consumableItemDumy}
+              />
+            }
+          />
         </div>
       )}
 
       <div className="flex flex-col gap-3 w-full">
-        {
-          accommodationDumy.map((accommodation: AccommodationModel) => (
-            <Collapse title={accommodation.alias} children={<h1>Testando</h1>}/>
-          ))
-        }
+        {accommodationDumy.map((accommodation: AccommodationModel) => (
+          <Collapse title={accommodation.alias} children={<h1>Testando</h1>} />
+        ))}
       </div>
     </div>
   );
