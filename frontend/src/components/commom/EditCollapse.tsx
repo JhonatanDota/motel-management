ad6891c2f43a03ca38d898f4787336837de1a060
jhonatan <j.azevedo@ctrlplay.com.br>
2023-11-26 16:32:31 -0300

    add transitions

--- frontend/src/components/commom/EditCollapse.tsx
+++ frontend/src/components/commom/EditCollapse.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useState } from "react";
 import DropDownAnimation from "./DropDownAnimation";
+import { FaCaretUp } from "react-icons/fa6";
 
 type CollapseProps = {
   title: string;
@@ -12,11 +13,25 @@ export default function EditCollapse(props: CollapseProps) {
   const [isOpen, setOpen] = useState<boolean>(false);
 
   return (
-    <div className="flex flex-col gap-4 bg-[#111827] p-4">
-      <div onClick={() => setOpen(!isOpen)} className="cursor-pointer">
+    <div
+      className={`flex flex-col bg-[#111827] p-4 transition-all duration-300 ${
+        isOpen ? "gap-4" : "gap-0"
+      }`}
+    >
+      <div
+        onClick={() => setOpen(!isOpen)}
+        className="flex justify-between cursor-pointer"
+      >
         <div className="flex justify-between">
           <p className="text-white font-bold">{title}</p>
         </div>
+        <button
+          className={`text-2xl text-white transition-transform duration-300 ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+        >
+          <FaCaretUp />
+        </button>
       </div>
 
       <DropDownAnimation isOpen={isOpen} heightValue="auto">
--- frontend/src/pages/ConsumableItem.tsx
+++ frontend/src/pages/ConsumableItem.tsx
@@ -1,7 +1,11 @@
+import PageContainer from "../components/pages/PageContainer";
+import PageTitle from "../components/pages/PageTitle";
+
 export default function ConsumableItem() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold tracking-wider">Acomodações</h1>
-    </div>
+    <PageContainer>
+      <PageTitle title="Consumíveis" />
+      
+    </PageContainer>
   );
 }
