130fc24989ecda3952c252d1b15cdfacb7837c98
jhonatan <j.azevedo@ctrlplay.com.br>
2023-12-16 12:04:24 -0300

    new design menu

--- frontend/src/components/menu/Menu.tsx
+++ frontend/src/components/menu/Menu.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import MenuItem from "./MenuItem";
+
+import { TiThMenu } from "react-icons/ti";
 
 import {
   FaCaretRight,
@@ -16,18 +19,42 @@ type MenuProps = {
 export default function Menu(props: MenuProps) {
   const { open, setOpen } = props;
 
+  const [isMobile, setIsMobile] = useState<boolean>(true);
+
+  function handleWindowSizeChange(): void {
+    setIsMobile(window.innerWidth <= 768);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
   return (
     <div
-      className={`${
-        open ? "w-20 md:w-24" : "w-12 md:w-16"
-      } flex flex-col items-center bg-[#111827] h-screen sticky top-0 px-7 py-12 transition-all duration-200`}
+      className={`flex flex-col items-center bg-[#111827] h-screen transition-all duration-200 py-12 ${
+        isMobile && !open
+          ? "w-0 px-0 max-w-0 overflow-hidden"
+          : `sticky top-0 px-7 ${open ? "w-20 md:w-24" : "w-12 md:w-16"}`
+      }`}
     >
-      <button
-        onClick={() => setOpen(!open)}
-        className="absolute top-2 -right-4 md:-right-5 text-3xl md:text-4xl"
-      >
-        {open ? <FaCaretLeft fill="red" /> : <FaCaretRight fill="red" />}
-      </button>
+      {isMobile ? (
+        <button
+          onClick={() => setOpen(!open)}
+          className="absolute right-3 top-3 text-2xl"
+        >
+          <TiThMenu fill={`${open ? "white" : "black"}`} />
+        </button>
+      ) : (
+        <button
+          onClick={() => setOpen(!open)}
+          className="absolute top-2 -right-4 md:-right-5 text-3xl md:text-4xl"
+        >
+          {open ? <FaCaretLeft fill="red" /> : <FaCaretRight fill="red" />}
+        </button>
+      )}
 
       <div className="flex flex-col gap-y-10 md:gap-y-20 mt-8 max-w-full">
         <MenuItem route="/" isMenuOpen={open} icon={FaHouse} title="Início" />
