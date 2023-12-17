8a31ab9dd24caefb3a871bbe12b015698399ea16
jhonatan <j.azevedo@ctrlplay.com.br>
2023-12-17 10:16:46 -0300

    reuse styles

--- frontend/src/components/commom/FormCurrency.tsx
+++ frontend/src/components/commom/FormCurrency.tsx
@@ -22,10 +22,10 @@ export default function FormCurrency(props: FormCurrencyProps) {
 
   return (
     <div className="flex flex-col gap-3 md:gap-5">
-      <label className="text-lg md:text-2xl font-bold text-[#111827]">{label}</label>
+      <label className="label">{label}</label>
       <CurrencyInput
         intlConfig={{ locale: "pt-BR", currency: "BRL" }}
-        className="text-base md:text-2xl rounded-md border-4 border-[#111827] focus:border-[#F64E2B] text-black p-3 md:p-5 font-bold outline-none transition-colors duration-300"
+        className="input"
         defaultValue={value}
         onValueChange={(_, __, values) =>
           values != undefined ? handleValueChange(values) : null
